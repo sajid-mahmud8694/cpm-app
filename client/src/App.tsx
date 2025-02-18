90562b439c54de57bfba85f75fc22867c322628f
sajid-mahmud8694 <146579802+sajid-mahmud8694@users.noreply.github.com>
2025-02-19 00:36:15 +0600

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import NotFound from "@/pages/not-found";
+import { AuthProvider } from "./hooks/use-auth";
+import AuthPage from "@/pages/auth-page";
+import Dashboard from "@/pages/dashboard";
+import NewOrder from "@/pages/new-order";
+import { ProtectedRoute } from "./lib/protected-route";
+
+function Router() {
+  return (
+    <Switch>
+      <ProtectedRoute path="/" component={Dashboard} />
+      <ProtectedRoute path="/new-order" component={NewOrder} />
+      <Route path="/auth" component={AuthPage} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
